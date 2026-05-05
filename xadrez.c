720f7ad119cc70fc19b3a88bdec5ee1e83737711
gabriel-costabyte <gabrielcostarosacontato@gmail.com>
2026-05-05 05:28:51 +0000

    Fiz o desafio nível aventureiro

--- xadrez.c
+++ xadrez.c
@@ -11,29 +11,53 @@ int main() {
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    printf("Bispo:\n");
+
         while (Bispo <= 5)
         {
             printf("Cima, Direita\n");
             Bispo ++;
         }
-        
+    printf("\n");
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    printf("Torre:\n");
+
         do{
             printf("Direita\n");
             Torre++;
         } while (Torre <= 5);
+
+    printf("\n");
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    printf("Rainha:\n");
+
         for (Rainha; Rainha <= 8; Rainha++)
         {
             printf("Esquerda\n");
         }
         
+    printf("\n");  
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
+    printf("Cavalo:\n");
 
+        for (int Cavalo = 0; Cavalo < 1; Cavalo++)
+        {
+            int Pulo = 0;
+
+            while (Pulo < 2)
+            {
+                printf("Baixo, ");
+                Pulo++;
+            }
+           
+                printf("Esquerda\n");
+
+        } 
+        
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
     // Exemplo: Crie uma função recursiva para o movimento do Bispo.
